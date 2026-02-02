01f910efa342ac64cec4adc5ea04f8fe36270546
hijabicoder12 <thecodingzoine53@gmail.com>
2026-02-03 01:13:53 +0500

    fDay1_pythonchallenge

---
+++ day1_python.py
@@ -0,0 +1,35 @@
+# Day 1 of days of 30 challenge 
+# What is print ?
+# print is a function 
+print("Hello hijabi") 
+
+# Try printing multiple things
+print("I am learning Python")
+print("Python is easy!")
+print("I will become a pro in 2026")
+
+# Print numbers
+print(2026)
+print(100)
+
+# Print calculations
+print(5 + 3)
+print(10 * 2)
+
+# Print empty lines
+print()
+print("This has a blank line above!")
+
+
+# Variables - storing information
+name = "hijabicoder"
+age = 20
+goal = "Python Pro"
+
+print(name)
+print(age)
+print(goal)
+
+# Combine text and variables
+print("My name is", name)
+print("My goal is to become a", goal, "in 2026!")
